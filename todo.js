f8178a5fbd49111b174247a2bf5459895be47a58
ItsmePatrice <112737835+ItsmePatrice@users.noreply.github.com>
2023-11-07 22:30:51 -0500

    html form element added with icons. Javascript function added to save the todos

---
+++ todo.js
@@ -0,0 +1,38 @@
+// SELECT ELEMENTS
+const form = document.getElementById("todoform");
+const todoInput = document.getElementById("newtodo");
+
+let todos = [];
+
+// FORM SUBMIST
+form.addEventListener("submit", function (event) {
+  event.preventDefault(); // prevent the page from reloading
+
+  saveTodo();
+});
+
+// SAVE TODO
+function saveTodo() {
+  const todoValue = todoInput.value;
+
+  // Check if the todo is empty
+  const isEmpty = todoValue === " ";
+
+  //check for duplicate todos
+  const isDuplicate = todos.some((todo) => todo.value.toUpperCase() === todoValue.toUpperCase());
+
+
+  if (isEmpty) {
+    alert("Todo is empty");
+  } else if(isDuplicate) {
+    alert("Todo already exists.");
+  } else {
+    todos.push({
+    value: todoValue,
+    checked: false,
+    color: "#" + Math.floor(Math.random()*16777215).toString(16);
+  });
+    todoInput.value = " ";
+  }
+
+}
